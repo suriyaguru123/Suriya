08aedcac50265fcd7ee3634f456bdcf6bb234ed1
suriyaguru123 <129590156+suriyaguru123@users.noreply.github.com>
2023-04-01 15:24:00 +0530

    Add files via upload
    Week1

---
+++ day1/Variable.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Variable {
+	public static void main(String[] args) {
+		int num1 =25;
+		boolean a=true;
+		String S="suriya";
+		System.out.println(num1);
+		System.out.println(a);
+		System.out.println(S);				
+	}
+
+}
